656b7c4911b8733e858fe1c2264cb048231cdbe1
Shabad Lamba <shabadlamba@gmail.com>
2015-08-10 13:00:43 +0530

    first commit

---
+++ myproject/main/creditcard.java
@@ -0,0 +1,10 @@
+public class creditcard {
+	public static void main(String[] args){
+		int myInt = 0;
+		char dollar = '\u0024';
+		System.out.print(dollar);
+		System.out.println("Interger value: " + myInt);
+		System.out.println("String");
+	}
+	
+}
